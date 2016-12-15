f9c25ac5c722a236cf128e6d790416ad7f7a8fa7
hyeonuk <hyeonuk.yoon@gmail.com>
2016-12-15 16:45:28 +0900

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,22 @@
-> if(!file.exists("./data")){dir.create("./data")}
+
+# Download data
+>if(!file.exists("./data")){dir.create("./data")}
 > fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 > download.file(fileUrl,destfile="./data/Dataset.zip")
-URL 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'�� �õ��մϴ�
+URL 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'À» ½ÃµµÇÕ´Ï´Ù
 Content type 'application/zip' length 62556944 bytes (59.7 MB)
 downloaded 59.7 MB
 
+# Unzip dataset
 > unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+# Merges the training and the test sets to create one data set
 > x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
 > y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
 > subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 > x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
 > y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
-> 
 > subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
-> 
 > features <- read.table('./data/UCI HAR Dataset/features.txt')
 > activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
 > colnames(x_train) <- features[,2] 
@@ -26,6 +29,8 @@ downloaded 59.7 MB
 > mrg_train <- cbind(y_train, subject_train, x_train)
 > mrg_test <- cbind(y_test, subject_test, x_test)
 > setAllInOne <- rbind(mrg_train, mrg_test)
+
+# Extracts only the measurements on the mean and standard deviation for each measurement
 > colNames <- colnames(setAllInOne)
 > mean_and_std <- (grepl("activityId" , colNames) | 
 +                  grepl("subjectId" , colNames) | 
@@ -33,9 +38,15 @@ downloaded 59.7 MB
 +                  grepl("std.." , colNames) 
 +                  )
 > setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+
+# Uses descriptive activity names to name the activities in the data set
 > setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
 +                               by='activityId',
 +                               all.x=TRUE)
+# Appropriately labels the data set with descriptive variable names
+
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 > secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
 > secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
 > write.table(secTidySet, "secTidySet.txt", row.name=FALSE)
